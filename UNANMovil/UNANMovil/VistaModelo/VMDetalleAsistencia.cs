d76bfbdd4fbbabaa2932edee3a7c2ed60df28054
13felixlopez <lfelixfermin@gmail.com>
2023-05-24 20:41:30 -0600

    ADD:Clase VMDetalleAsistencia UPDATE:Patalla DetalleAsistencia

---
+++ UNANMovil/UNANMovil/VistaModelo/VMDetalleAsistencia.cs
@@ -0,0 +1,40 @@
+﻿using Android.Content.Res;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace UNANMovil.VistaModelo
+{
+    public class VMDetalleAsistencia:BaseViewModel
+    {
+        #region VARIABLES
+        string identificacion;
+        #endregion
+        #region CONSTRUCTOR
+        public VMDetalleAsistencia(INavigation navigation)
+        {
+            Navigation = navigation;
+            volvercomamd = new Command(async () => await Volver());
+        }
+        #endregion
+        #region OBJETOS 
+        public string Identificacion
+        {
+            get { return identificacion; }
+            set { SetValue(ref identificacion, value); }
+        }
+
+        #endregion
+        #region PROCESOS
+        private async Task Volver()
+        {
+            await Navigation.PopAsync();
+        }
+        #endregion
+        #region COMANDOS
+        public Command volvercomamd { get; }
+        #endregion
+    }
+}
--- UNANMovil/UNANMovil/Vistas/DetalleAsistencia.xaml.cs
+++ UNANMovil/UNANMovil/Vistas/DetalleAsistencia.xaml.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using UNANMovil.VistaModelo;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -15,6 +15,7 @@ namespace UNANMovil.Vistas
 		public Registro ()
 		{
 			InitializeComponent ();
+			BindingContext = new VMDetalleAsistencia(Navigation);
 		}
 	}
 }
