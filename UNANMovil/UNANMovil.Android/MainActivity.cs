5c141128a23b649d2fd434d00cb6c147d46c4e04
13felixlopez <lfelixfermin@gmail.com>
2023-06-01 08:58:33 -0600

    ADD:Icono de la app UPDATE:Se cambiaron aspectos de tamaño de las pantallas

--- UNANMovil/UNANMovil.Android/MainActivity.cs
+++ UNANMovil/UNANMovil.Android/MainActivity.cs
@@ -7,7 +7,7 @@ using Android.OS;
 
 namespace UNANMovil.Droid
 {
-    [Activity(Label = "UNANMovil", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize, ScreenOrientation=ScreenOrientation.Portrait )]
+    [Activity(Label = "UNANMovil", Icon = "@mipmap/Unan1", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize, ScreenOrientation=ScreenOrientation.Portrait )]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
--- UNANMovil/UNANMovil.Android/Resources/Resource.designer.cs
+++ UNANMovil/UNANMovil.Android/Resources/Resource.designer.cs
@@ -21441,6 +21441,9 @@ namespace UNANMovil.Droid
 			// aapt resource value: 0x7F0C0002
 			public const int launcher_foreground = 2131492866;
 			
+			// aapt resource value: 0x7F0C0003
+			public const int Unan1 = 2131492867;
+			
 			static Mipmap()
 			{
 				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
